47a0b85b5321faf9a343ca4e4c988459e34ff5ed
beytullahagitakgun <84814232+beytullahagitakgun@users.noreply.github.com>
2022-06-20 17:17:45 +0300

    Inheritance

--- objectoriented/ObjectFactory.java
+++ objectoriented/ObjectFactory.java
@@ -15,6 +15,24 @@ public class ObjectFactory {
 		secondPen.color = "Pembe";
 		secondPen.height = 15;
 		secondPen.writeMessage(secondPen.color + " " + secondPen.height);
+		
+		
+		//Inheritance
+		
+		Car car = new Car();
+		car.numberOfSeats = 5;
+		car.numberOfWheels = 4;
+		
+		Bicycle bicycle = new Bicycle();
+		bicycle.numberOfSeats = 1;
+		bicycle.numberOfWheels = 2;
+		
+		//Bu þekilde Vehicle class ýndaki özellikleri Car ve Bicycle içinde de kullanabildik
+		//Bu þekilde kod tekrarýndan sakýnmýþ oldum.
+		
+		//Superclass örnek
+		Vehicle car2 = new Car();
+		
 	}
 
 }
